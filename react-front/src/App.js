1d59072ddcdc95f99f1f0ee737be41a2a7501a17
Wanlin-Lu <luwanlin@126.com>
2020-09-04 11:29:29 +0800

    add ali-oss, antd & config, uuid

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.less';
+
+import { Button } from 'antd'
 
 function App() {
   return (
     <div className="App">
       <h1>React img Slider</h1>
+      <Button type="primary">upload</Button>
     </div>
   );
 }
