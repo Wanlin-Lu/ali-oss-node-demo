3fdc994533500a5580bbd9e50ec34b22d35d99ee
Wanlin-Lu <luwanlin@126.com>
2020-09-07 22:19:05 +0800

    imageDAO

---
+++ server/dao/imageDao.js
@@ -0,0 +1,33 @@
+let images
+
+const injectDB = async (mdc) => {
+  if (images) return
+  try {
+    images = await mdc.db("oss_demo").collection("images");
+  } catch (e) {
+    console.error(`Unable to establish collection handles in userDAO: ${e}`);
+  }
+}
+
+const getImages = async () => {
+  try {
+    return await images.find().toArray();
+  } catch (e) {
+    console.error("Unable to get images");
+    return { error: e}
+  }
+}
+
+const postImages = async (images) => {
+  try {
+    await images.insertMany(images)
+    return {success: true}
+  } catch (e) {
+    console.error('Unable to post images')
+    return { error: e }
+  }
+}
+
+exports.injectDB = injectDB
+exports.getImages = getImages
+exports.postImages = postImages
