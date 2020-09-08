890113252f6b44e9410878bc2e90c4c56ec69554
Wanlin-Lu <luwanlin@126.com>
2020-09-08 23:28:02 +0800

    Uploader

--- react-front/src/containers/Uploader/index.js
+++ react-front/src/containers/Uploader/index.js
@@ -1,11 +1,14 @@
 import React, { useState, useRef } from 'react'
+import { v4 as uuidv4 } from "uuid";
 import { useAlioss } from '../../hooks/oss-hook'
+import { useHttpClient } from '../../hooks/http-hook'
 
 import './style.less'
 
 const Uploader = () => {
   let [files, setFiles] = useState(null);
-  const { upload, uploaded } = useAlioss()
+  const { upload } = useAlioss()
+  const { sendRequest } = useHttpClient()
   const filePickRef = useRef()
 
   const handleChange = e => {
@@ -16,11 +19,35 @@ const Uploader = () => {
   };
 
   const handleUpload = async () => {
+    let nameList = []
+    files.forEach(file => {
+      file.uname = `${uuidv4()}-${file.name}`;
+      nameList.push({img:file.uname})
+    })
     await upload(files)
-    if (uploaded) {
-      console.log(uploaded);//! get uploaded name array
-    }
+    console.log("upload return uploaded:", nameList)
+    const images = {'images':nameList}
+    await postImages(images);
   };
+
+  async function postImages(images) {
+    console.log(images)
+    console.log(JSON.stringify(images))
+    try {
+      const dbResponse = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/upload/images`,
+        {
+          method: "POST",
+          body: JSON.stringify(images),
+          headers:{"Content-Type": "application/json",}
+        }
+      );
+      console.log(dbResponse);
+    } catch (e) {
+      console.log("uploaded post failed");
+    }
+  }
+  
   return (
     <div className="uploader">
       <input
