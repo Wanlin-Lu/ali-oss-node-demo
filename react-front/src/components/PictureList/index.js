219b3151fc315a1b49aadd4e0c787c63e9a9893e
Wanlin-Lu <luwanlin@126.com>
2020-09-10 23:34:35 +0800

    debug: async/await-array.forEach()

--- react-front/src/components/PictureList/index.js
+++ react-front/src/components/PictureList/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const PictureList = ({ imgs }) => {
+const PictureList = props => {
+  console.log(props)
   return (
     <ul>
-      {imgs.map(i => <img key={i._id} src={i.img} />)}
+      {/* imgs.length !== 0 ? imgs.map(i => <img key={i._id} src={i.src} alt="pic" />) : <h1>loading...</h1> */}
+      {props.imgs.pl && props.imgs.pl.map((i) => (
+        <img key={i._id} src={i.src} alt="pic" />
+      ))}
     </ul>
-  )
+  );
 }
 
 export default PictureList
--- react-front/src/containers/Pictures/index.js
+++ react-front/src/containers/Pictures/index.js
@@ -1,16 +1,21 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef } from 'react'
 import { useAlioss } from '../../hooks/oss-hook'
-import { useHttpClient } from '../../hooks/http-hook'
 import PictureList from '../../components/PictureList'
 
+import { useHttpClient } from '../../hooks/http-hook'
 import {Carousel, Image} from 'antd'
 
 import './style.less'
 
 const Pictures = () => {
-  const [results, setResults] = useState({})
-  const [images, setImages] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [signatured, setSignatured] = useState(false)
+  const [results, setResults] = useState()
+  const [images, setImages] = useState({'pl':[]})
   const { allowUrl } = useAlioss()
+  // ! useRef.current to sequency the operations
+  const resultsDoSetRef = useRef(false)
+  const picListRef = useRef()
 
   useEffect(() => {
     getImages();
@@ -24,31 +29,94 @@ const Pictures = () => {
 
       const resu = await dbResponse.json();
 
-      mySetResult(resu);
+      setResults(resu);
+      resultsDoSetRef.current = true
 
-      resu.imgs.forEach((result) => {
-        allowUrl(result.img).then((res) => {
-          result.img = res;
-        });
-      });
+      // const temImgs = []
+      // await resu.imgs.forEach((result) => {
+      //   allowUrl(result.img).then((res) => {
+      //     result.src = res;
+      //     temImgs.push(result)
+      //   });
+      // });
 
-      mySetImages(resu.imgs);
+      // setImages(temImgs);
     } catch (e) {
-      console.log("get images failed");
+      console.log("get images failed")
+    } finally {
+      setLoading(false)
+      // console.log("get images and signatured")
+      console.log("get images done!")
     }
   }
   
-  const mySetImages = (images) => {
-    setImages(images)
+  async function signatureUrl(raw) {
+    setSignatured(false)
+    try {
+
+      let tempImgs = []
+
+      raw.imgs.forEach((r) => {
+        allowUrl(r.img).then((res) => {
+          r.src = res;
+          tempImgs.push(r)
+          console.log("r",r)
+          console.log('r.src',r['src'])
+        });
+      });
+
+      console.log("tempImgs",tempImgs)
+
+      setImages({ 'pl': tempImgs });
+    } catch (e) {
+      console.log("signature failed",e)
+    } finally {
+      setSignatured(true)
+      console.log("signature done!")
+    }
   }
 
-  const mySetResult = (results) => {
-    setResults(results)
+  function signatureUrl1(raw, callback) {
+    let tempImgs = []
+    let expecting = raw.imgs.length
+    raw.imgs.forEach(function (entry, index) {
+      signature(entry, function (result) {
+        tempImgs[index] = result
+        if (--expecting === 0) {
+          callback({ pl: tempImgs });
+        }
+      })
+    })
   }
+
+  function signature(item, callback) {
+    allowUrl(item.img).then(res => {
+      item.src = res
+      callback(item)
+    })
+  }
+
+  useEffect(() => {
+    if (resultsDoSetRef.current) {
+      resultsDoSetRef.current = false
+      // signatureUrl(results);
+      signatureUrl1(results, setImages)
+    }
+  }, [results])
+  
+  // useEffect(() => {
+  //   const setPicList = i => {
+  //     return (
+  //       <PictureList imgs={i} />
+  //     )
+  //   }
+  //   picListRef.current = setPicList(images)
+  // }, [images])
   
   return (
     <div className="picture">
-      <PictureList imgs={images} />
+      {loading && !signatured ? <h1>Loading</h1> : <PictureList imgs={images} />}
+      {/* !!picListRef.current && picListRef.current */}
     </div>
   );
 };
